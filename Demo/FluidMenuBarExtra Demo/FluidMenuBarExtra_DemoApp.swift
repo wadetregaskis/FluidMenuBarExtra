8a50b4c71f5406c503b897a8d397ff523657e0dc
Wade Tregaskis <wadetregaskis@mac.com>
2023-04-24 13:30:10 -0700

    Now use the same 'demo' view in the Settings window as in the pop-up window, for simplicity.
    This is also a convenient way to check if there's any difference in behaviour between them.

--- Demo/FluidMenuBarExtra Demo/FluidMenuBarExtra_DemoApp.swift
+++ Demo/FluidMenuBarExtra Demo/FluidMenuBarExtra_DemoApp.swift
@@ -17,25 +17,20 @@ private struct FluidMenuBarExtra_DemoApp: App {
     var body: some Scene {
         Settings() {
             Form {
-                Toggle("Show menubar extra", isOn: $showMenuBarExtra)
-
-                ForEach($extraButtons, id: \.self) { $button in
-                    Button("No more buttons!") {
-                        extraButtons.removeLast()
-                    }
-                }
-            }.padding(20)
-                .fixedSize()
+                DemoView(showMenuBarExtra: $showMenuBarExtra,
+                         extraButtons: $extraButtons)
+            }.fixedSize()
         }.windowResizability(.contentSize)
 
         FluidMenuBarExtra("Demo", systemImage: "chevron.down.circle", isInserted: $showMenuBarExtra) {
             /// IMPORTANT:  If you have dynamic content (as this example does, with bindings to state variables) you must define your view in a separate struct, not inline right here.  Otherwise any updates to your state variables won't be reflected in your views.  This appears to be a SwiftUI bug (or bizarre limitation).
-            DemoMenuItemView(showMenuBarExtra: $showMenuBarExtra, extraButtons: $extraButtons)
+            DemoView(showMenuBarExtra: $showMenuBarExtra,
+                     extraButtons: $extraButtons)
         }
     }
 }
 
-fileprivate struct DemoMenuItemView: View {
+fileprivate struct DemoView: View {
     @Binding var showMenuBarExtra: Bool
     @Binding var extraButtons: [Int]
 
